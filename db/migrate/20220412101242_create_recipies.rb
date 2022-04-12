349e44374211d7562406fb85502a89ed75497448
mahbubul14 <mahbub21alam2017@gmail.com>
2022-04-12 16:45:20 +0600

    Create controller for recipies

--- db/migrate/20220412101242_create_recipies.rb
+++ db/migrate/20220412101242_create_recipies.rb
@@ -1,8 +1,12 @@
 class CreateRecipies < ActiveRecord::Migration[7.0]
   def change
     create_table :recipies do |t|
-
+      t.string :name
+      t.string :measurement_unit
+      t.integer :preparation_time
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
+    add_index :foods, :price
   end
 end
