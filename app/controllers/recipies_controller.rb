48b97e7a0bd07d593cdfb7c619bf0358f33655ac
mahbubul14 <mahbub21alam2017@gmail.com>
2022-04-13 11:38:04 +0600

    Fix the error with params

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -2,18 +2,23 @@ class RecipiesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_recipy, only: %i[show edit update destroy]
 
+  # GET /recipies or /recipies.json
   def index
     @recipies = Recipy.all
   end
 
+  # GET /recipies/1 or /recipies/1.json
   def show; end
 
+  # GET /recipies/new
   def new
     @recipy = Recipy.new
   end
 
+  # GET /recipies/1/edit
   def edit; end
 
+  # POST /recipies or /recipies.json
   def create
     user = current_user
     @recipy = user.recipies.new(recipy_params)
@@ -28,10 +33,11 @@ class RecipiesController < ApplicationController
     end
   end
 
+  # PATCH/PUT /recipies/1 or /recipies/1.json
   def update
     respond_to do |format|
       if @recipy.update(recipy_params)
-        format.html { redirect_to recipy_url(@recipy), notice: 'recipy was successfully updated.' }
+        format.html { redirect_to recipy_url(@recipy), notice: 'Recipy was successfully updated.' }
         format.json { render :show, status: :ok, location: @recipy }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -40,6 +46,7 @@ class RecipiesController < ApplicationController
     end
   end
 
+  # DELETE /recipies/1 or /recipies/1.json
   def destroy
     @recipy.destroy
 
@@ -53,11 +60,11 @@ class RecipiesController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_recipy
-    @recipy = recipy.find(params[:id])
+    @recipy = Recipy.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
   def recipy_params
-    params.require(:recipy).permit(:name, :measurement_unit, :price)
+    params.require(:recipy).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 end
