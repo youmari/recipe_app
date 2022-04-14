0c0f1b5d440ee313d82384c24533876e11694d5a
mahbubul14 <mahbub21alam2017@gmail.com>
2022-04-14 16:35:47 +0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'shopping_lists/index'
-  get 'shopping_lists/show'
   devise_for :users
   resources :foods
   resources :recipies
   resources :public_recipies
+  resources :shopping_lists
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "recipies#index"
+  root "foods#index"
 end
