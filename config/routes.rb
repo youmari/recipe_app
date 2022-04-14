2c47d1c33e4191ad45d67cba16b13ee42830191e
mahbubul14 <mahbub21alam2017@gmail.com>
2022-04-14 13:56:41 +0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'public_recipies/index'
-  get 'public_recipies/show'
   devise_for :users
   resources :foods
   resources :recipies
+  resources :public_recipies
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
