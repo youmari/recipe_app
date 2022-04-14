34f0bd34deabf20e7aa7d2a7def20f4282c94b6c
mahbubul14 <mahbub21alam2017@gmail.com>
2022-04-14 20:09:58 +0600

    Update shopping list database

--- db/migrate/20220414103347_create_shopping_lists.rb
+++ db/migrate/20220414103347_create_shopping_lists.rb
@@ -1,7 +1,9 @@
 class CreateShoppingLists < ActiveRecord::Migration[7.0]
   def change
     create_table :shopping_lists do |t|
-
+      t.string :food
+      t.string :quantity
+      t.integer :price
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_14_081916) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_14_103347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_14_081916) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_recipies_on_user_id"
+  end
+
+  create_table "shopping_lists", force: :cascade do |t|
+    t.string "food"
+    t.string "quantity"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
