587ddb4b52648b1c592e0a0a98acb00649a6bc8a
mahbubul14 <mahbub21alam2017@gmail.com>
2022-04-14 19:25:14 +0600

    Update actions of controller

--- app/controllers/public_recipies_controller.rb
+++ app/controllers/public_recipies_controller.rb
@@ -1,7 +1,7 @@
 class PublicRecipiesController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :set_food, only: %i[ show ]
+  before_action :set_public_recipie, only: %i[ show ]
   def index
     @public_recipie = PublicRecipe.all
   end
--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,5 +1,8 @@
 class ShoppingListsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_shopping_list, only: %i[show]
   def index
+    @public_recipie = PublicRecipe.all
   end
 
   def show
